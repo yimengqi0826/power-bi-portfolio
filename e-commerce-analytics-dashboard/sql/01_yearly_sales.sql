51920ba3ebba3f8260731718ef0f5628a991d497
yimengqi0826 <yimengqi99@gmail.com>
2026-04-19 15:51:18 +0200

    Update and rename 01_kpi_analysis.sql to 01_yearly_sales.sql

--- e-commerce-analytics-dashboard/sql/01_yearly_sales.sql
+++ e-commerce-analytics-dashboard/sql/01_yearly_sales.sql
@@ -1,7 +1,10 @@
--- KPI Analysis
+-- Annual sales and profit performance
 
 SELECT 
+    order_year,
     COUNT(DISTINCT Order_ID) AS total_orders,
     SUM(Total_Sales) AS total_sales,
     SUM(Profit) AS total_profit
-FROM stg_sales;
+FROM stg_sales
+GROUP BY order_year
+ORDER BY order_year;
